9185edce3f9bab408f01726a489f49c337d2d49f
godkorea <dlfnek3@gmail.com>
2024-04-10 23:04:11 +0900

    delete image

--- js/background.js
+++ js/background.js
@@ -1,9 +1,9 @@
 const images = [
-    "0.png",
-    "1.png",
-    "2.png",
-    "3.png",
-    "4.png"
+    //"0.png",
+    //"1.png",
+    //"2.png",
+    //"3.png",
+    //"4.png"
 ]
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
